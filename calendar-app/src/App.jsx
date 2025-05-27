02f3d10572add9af39f754b7faa42ebd02324aac
reddy-gowthami <reddemgowthamireddy@gmail.com>
2025-05-27 12:49:38 +0530

    initial commit

---
+++ calendar-app/src/App.jsx
@@ -0,0 +1,11 @@
+import CalendarApp from "./Components/CalendarApp"
+import './Components/CalendarApp.css'
+const App = () => {
+  return (
+    <div className='container'>
+      <CalendarApp/>
+    </div>
+  )
+}
+
+export default App
